3bb81ca31fbb2992e268dcc2410c781204ecccb5
Anders Jonsson <andersh_hotmail.com>
2021-02-24 22:33:19 +0100

    add RevealOnScroll module to project

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,7 @@
 import '../styles/styles.css';
+import RevealOnScroll from './modules/RevealOnScroll';
 
+new RevealOnScroll(document.querySelectorAll('.testimonial'), 65);
 
 if (module.hot) {
   module.hot.accept();
