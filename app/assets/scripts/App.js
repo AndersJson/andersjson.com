52e7ae57c08bfcb6f42476fbe55f89c33fee57a6
Anders Jonsson <andersh_hotmail.com>
2021-02-25 12:40:46 +0100

    fix reveal on scroll for about section and adjust the position of the about-section

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,11 @@
 import '../styles/styles.css';
 import RevealOnScroll from './modules/RevealOnScroll';
 
-new RevealOnScroll(document.querySelectorAll('.testimonial'), 65);
+const revealAbout = new RevealOnScroll(document.querySelectorAll('.about__wrapper'), 65, "reveal-element", "reveal-element--visible");
+
+setTimeout(()=>{
+  revealAbout.calcCaller();
+}, 1500);
 
 if (module.hot) {
   module.hot.accept();
--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -2,42 +2,40 @@ import throttle from 'lodash/throttle'
 import debounce from 'lodash/debounce'
 
 class RevealOnScroll {
-  constructor(els, thresholdPercent) {
-    this.thresholdPercent = thresholdPercent
-    this.itemsToReveal = els
-    this.browserHeight = window.innerHeight
-    this.hideInitially()
-    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this)
-    this.events()
-    setTimeout(()=>{
-      this.calcCaller();
-    }, 1000);
+  constructor(elements, thresholdPercent, hideClass, showClass) {
+    this.hideClass = hideClass;
+    this.showClass = showClass;
+    this.thresholdPercent = thresholdPercent;
+    this.itemsToReveal = elements;
+    this.browserHeight = window.innerHeight;
+    this.hideInitially();
+    this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
+    this.events();
   }
 
   events() {
-    window.addEventListener("scroll", this.scrollThrottle)
+    window.addEventListener("scroll", this.scrollThrottle);
     window.addEventListener("resize", debounce(() => {
-      console.log("Resize just ran")
-      this.browserHeight = window.innerHeight
-    }, 500))
+      this.browserHeight = window.innerHeight;
+    }, 500));
   }
 
   calcCaller() {
     this.itemsToReveal.forEach(el => {
       if (el.isRevealed == false) {
-        this.calculateIfScrolledTo(el)
+        this.calculateIfScrolledTo(el);
       }
     })
   }
 
   calculateIfScrolledTo(el) {
     if (window.scrollY + this.browserHeight > el.offsetTop) {
-      let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100
+      let scrollPercent = (el.getBoundingClientRect().top / this.browserHeight) * 100;
       if (scrollPercent < this.thresholdPercent) {
-        el.classList.add("reveal-item--is-visible")
-        el.isRevealed = true
+        el.classList.add(this.showClass);
+        el.isRevealed = true;
         if (el.isLastItem) {
-          window.removeEventListener("scroll", this.scrollThrottle)
+          window.removeEventListener("scroll", this.scrollThrottle);
         }
       }
     }
@@ -45,10 +43,10 @@ class RevealOnScroll {
 
   hideInitially() {
     this.itemsToReveal.forEach(el => {
-      el.classList.add("reveal-item")
-      el.isRevealed = false
+      el.classList.add(this.hideClass);
+      el.isRevealed = false;
     })
-    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true
+    this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
   }
 }
 
